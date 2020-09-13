7f885dad15f1889a92632ef93cd0d559be88a66c
Michael <969149642@qq.com>
2020-09-13 19:16:03 +0800

    start add

--- js/js.js
+++ js/js.js
@@ -1,6 +1,8 @@
 $(function () {
-    $('#submit').click(() => {
+    $('#submit').click(function () {
         formData = $("form").serialize()
+        console.log(formData);
+
         id = $('#id').val()
         pwd = $('#pwd').val()
         key = $('#key').val()
@@ -12,7 +14,6 @@ $(function () {
             alert("请填写正确密码")
             return
         } else if (!/^[0-9A-Za-z]{45,60}$/.test(key)) {
-            console.log("不符合");
             $('#key').val("")
             $('#key').focus()
             alert("key格式错误，请核对后填入")
@@ -20,17 +21,17 @@ $(function () {
         } else {
             $.ajax({
                 type: "GET",
-                url: " http://gpq77h.natappfree.cc/add",
+                url: "  http://ajsn8a.natappfree.cc/add",
                 data: formData,
                 dataType: "jsonp",
                 jsonp: "callback",
                 async: true,
-                success: (res) => {
+                success: function (res) {
                     if (res.code === 200) {
                         alert("您的信息已经提交成功！")
                     }
                 },
-                error: (err) => {
+                error: function (err) {
                     console.log("err:", err);
                     alert("提交通道已关闭！请把信息发送到邮箱")
                 },
